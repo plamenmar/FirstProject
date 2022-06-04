8f6b571b3dc2d27a7630851fe31d9f2715d52582
Plamen Marinov <plamenmarinov@Plamens-MacBook-Pro.local>
2022-06-04 07:38:29 -0500

    Initial commit

---
+++ src/FirstClass.java
@@ -0,0 +1,9 @@
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        int a=6;
+        int b=8;
+        int sum=a+b;
+        System.out.println(sum);
+    }
+}
